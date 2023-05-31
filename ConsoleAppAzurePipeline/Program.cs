37274783adcc565a476946d64c02e80b6cbee654
Anshul Dahiya <anshul.dahiya@ihsmarkit.com>
2023-05-31 13:03:19 +0530

    First Commit

--- ConsoleAppAzurePipeline/Program.cs
+++ ConsoleAppAzurePipeline/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World from C#!");
+using System.Reflection;
+
+Console.WriteLine("First Commit");
+string path = Directory.GetCurrentDirectory();
+
+path += "\\abc.txt";
+
+if (File.Exists(path))
+{
+    File.Delete(path);
+}
+
+using (StreamWriter sw = File.CreateText(path))
+{
+    sw.WriteLine("First Commit");
+}
